Tried adding 1/2 power on determinant of resolutio coefficient as there appears to be a mistake in the paper. Tracking still does not work. Moving to eliminating constant in resolution covariance and using simple gaussin form instead.

dataAssociation.py
@@ -7,6 +7,7 @@ from scipy.stats import poisson, multivariate_normal
 from scipy.linalg import sqrtm
 from math import factorial
 
+sqrt = np.sqrt
 
 from copy import copy
 from collections import deque
@@ -331,11 +332,12 @@ class JPDAF:
 
         return valid_events
 
-
 ########################
 ###### end JPDAF #######
 ########################
 
+########################################################################################################################
+########################################################################################################################
 
 class JPDAFMerged:
 
@@ -388,7 +390,7 @@ class JPDAFMerged:
             gda.calculate_association_probabilities(self._clutter_density, self._FOV, self.sensor._detection_prob)
             gda.build_C_k_matrices()
             gda.perform_measurement_update(full_belief, H_tilde, b_sigma, measurements, z_target_predict, self._FOV)
-            gda.perform_resolution_update(H_tilde, self.bearing_res)
+            gda.perform_resolution_update(H_tilde, self.bearing_res, z_target_predict)
 
         # Weights and gaussian beliefs calculated for all graphs, data associations and resolutions updates
         # Now perform moment matching on entire gaussian mixture.
@@ -655,7 +657,7 @@ class GraphDataAssociation:
         self.resolution_updated_beliefs = []
         self.resolution_updated_probabilities = []
 
-    def perform_resolution_update(self, H_tilde, bearing_res):
+    def perform_resolution_update(self, H_tilde, bearing_res, z_target_predict):
         """
         takes the measurement updated beliefs that are already stored and does a resolution update with the graph
         :param H_tilde:
@@ -676,14 +678,17 @@ class GraphDataAssociation:
                     continue
                 D_mat = self.graph.resolution_update_D_matrices[k]
                 update_sign = self.graph.edge_multipliers_sign[k]
-                inn_cov = (D_mat @ H_tilde) @ cov @ (D_mat @ H_tilde).transpose() + R_u * np.eye(self.graph.n_vertices)
-                gain = cov @ (D_mat @ H_tilde).transpose() @ np.linalg.inv(inn_cov)
-                mean_update = mean + gain @ (np.zeros((self.graph.n_vertices, 1)) - D_mat @ H_tilde @ mean)
+                inn_cov = (D_mat @ H_tilde) @ cov @ (D_mat @ H_tilde).T + R_u * np.eye(self.graph.n_vertices)
+                gain = cov @ (D_mat @ H_tilde).T @ np.linalg.inv(inn_cov)
+                #mean_update = mean + gain @ (np.zeros((self.graph.n_vertices, 1)) - D_mat @ H_tilde @ mean)
+                mean_update = mean + gain @ (np.zeros((self.graph.n_vertices, 1)) - D_mat @ z_target_predict)
                 cov_update = (np.eye(N_targets * y_dim) - gain @ D_mat @ H_tilde) @ cov
                 self.resolution_updated_beliefs.append(GaussianBelief(mean_update, cov_update))
-                probability_factor = np.linalg.det(2 * np.pi * R_u * np.eye(N_targets))
-                probability_factor = probability_factor * multivariate_normal.pdf(np.zeros(N_targets), np.ndarray.flatten(D_mat @(H_tilde @ mean)),
-                                                                              inn_cov)
+                probability_factor = sqrt(np.linalg.det(2 * np.pi * R_u * np.eye(N_targets)))
+                #probability_factor = probability_factor * multivariate_normal.pdf(np.zeros(N_targets), np.ndarray.flatten(D_mat @(H_tilde @ mean)),
+                                                                              #inn_cov)
+                probability_factor = probability_factor * multivariate_normal.pdf(np.zeros(N_targets), np.ndarray.flatten(D_mat @ (z_target_predict)),
+                                                                                  inn_cov)
                 self.resolution_updated_probabilities.append(update_sign * probability_factor *
                                                              self.measurement_updated_probabilities[association_index])
             association_index += 1

test/test_data_association_merged.py
@@ -17,7 +17,7 @@ print(restrict_angle(b0-b1), " radians, ", restrict_angle(b0-b1)*180/np.pi, " de
 yaml_file = 'config/init_info_planner.yaml'
 p = Parameters(yaml_file)
 
-if False:
+if True:
     angles = np.linspace(-30, 30)
     angles_radians = angles * pi/180
     bearing_res = p.unresolved_resolution
@@ -67,7 +67,7 @@ for kk in range(p.Tmax):
 
     print("Timstep: ", kk)
 
-file_name = 'JPDAF/merged/2_targ/2_targ_JPDAF_mask_5'
+file_name = 'JPDAF/merged/2_targ/2_targ_maksed_10'
 plotter.save_video(filename=file_name, fps=5)
 
 